f4a4ca0bb35df638e2401b4a0278d87d52ae13ae
james <jimmindungu3@gmail.com>
2024-09-13 21:12:51 +0300

    create footer

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import logo from "../assets/logo.svg";
+import { FaFacebook } from "react-icons/fa6";
+import { FaSquareXTwitter } from "react-icons/fa6";
+import { FaInstagramSquare } from "react-icons/fa";
+import { FaTiktok } from "react-icons/fa";
+
+const Footer = () => {
+  const currentYear = new Date().getFullYear();
+  return (
+    <div>
+      <div className="mx-auto max-w-6xl h-2 my-8 bg-bluegray"></div>
+      <div className="mx-auto my-6 max-w-6xl flex justify-between text-bluegray">
+        <div>
+          <img src={logo} alt="Logo" className="max-w-24 h-auto" />
+          <div className="text-sm mt-2">
+            &copy; {currentYear} Your Company Name. All rights reserved.
+          </div>
+        </div>
+        <div className="flex flex-col text-sm">
+          <div className="text-red-100 font-semibold mb-2 text-lg">Links</div>
+          <a href="#">About Us</a>
+          <a href="#">Privacy Policy</a>
+          <a href="#">Terms & Conditions</a>
+          <a href="#">Help Center</a>
+        </div>
+        <div>
+          <div className="text-red-100 font-semibold mb-2 text-lg">
+            Our Socials
+          </div>
+          <div className="flex justify-between">
+            <a href="#">
+              <FaFacebook />
+            </a>
+            <a href="#">
+              <FaSquareXTwitter />
+            </a>
+            <a href="#">
+              <FaInstagramSquare />
+            </a>
+            <a href="#">
+              <FaTiktok />
+            </a>
+          </div>
+        </div>
+        <div className="flex flex-col text-sm">
+          <div className="text-red-100 font-semibold mb-2 text-lg">
+            Contact
+          </div>
+          <div className="my-2 font-semibold">Physical Location</div>
+          <div>Kephanie House off Thika-Garissa Road</div>
+          <div>Tel: +254 717 055 495</div>
+          <div>email: jimmindungu3@gmail.com</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
