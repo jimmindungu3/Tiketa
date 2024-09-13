908bc48582e96de090a808d859017115d88eef8e
james <jimmindungu3@gmail.com>
2024-09-13 21:14:10 +0300

    configure tailwind colors

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,11 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      // transparent: "transparent",
+      // current: "currentColor",
+      white: "#eff6ee",
+      red: {
+        100: "#DD0426",
+        200: "#F02D3A",
+      },
+      blue: "#273043",
+      bluegray: "#9197AE",
+      silver: "#C0C0C0"
+    },
   },
   plugins: [],
-}
+};
