17a30b78c453d40d929bcf879770637e45c3612d
james <jimmindungu3@gmail.com>
2024-09-13 21:13:16 +0300

    create navbar

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import logo from "../assets/logo.svg";
+import { MdEventAvailable } from "react-icons/md";
+import { MdFlightTakeoff } from "react-icons/md";
+import { RiMovie2Line } from "react-icons/ri";
+import { MdHotel } from "react-icons/md";
+
+
+const Navbar = () => {
+  return (
+    <div className="bg-blue py-4 pb-48">
+      <div className="max-w-6xl mx-auto">
+        <div className="flex justify-between items-center">
+          <img src={logo} alt="logo" className="w-32 h-auto" />
+          <div className="flex text-white font-semibold">
+            <a className="flex items-center mx-4" href="#">
+              <MdEventAvailable className="mr-1" />
+              Events
+            </a>
+            <a className="flex items-center mx-4" href="#">
+              <RiMovie2Line className="mr-1" />
+              Movies
+            </a>
+            <a className="flex items-center mx-4" href="#">
+              <MdFlightTakeoff className="mr-1" />
+              Flights
+            </a>
+            <a className="flex items-center mx-4" href="#">
+              <MdHotel className="mr-1" />
+              Hotels
+            </a>
+          </div>
+          <div className="text-white font-semibold">
+            <button className="mx-2">Sign Up</button>
+            <button className="mx-2 bg-red-100 py-2 px-4 rounded-md">
+              Login
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
