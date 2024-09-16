64ec831d0f3d406e177fb4deeaf29163cadeff43
james <jimmindungu3@gmail.com>
2024-09-16 20:17:14 +0300

    add error messages to model

--- backend/models/UserSchema.js
+++ backend/models/UserSchema.js
@@ -1,18 +1,24 @@
 const mongoose = require("mongoose");
+const { isEmail } = require("validator");
 
 const userSchema = new mongoose.Schema({
   fullName: {
     type: String,
-    required: true,
+    required: [true, "Please enter full name"],
+    minlength: [4, "Full name must be 4 characters or more"],
+    uppercase: true
   },
   email: {
     type: String,
-    required: true,
-    unique: true,
+    unique: [true, "This email already in use"],
+    required: [true, "Please enter email address"],
+    validate: [isEmail, "Please enter a valid email"],
+    lowercase: true,
   },
   password: {
     type: String,
     required: true,
+    minlength: [6, "Password must be 6 characters or more"]
   },
 });
 
