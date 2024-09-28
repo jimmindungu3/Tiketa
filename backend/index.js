6ce5fd32272090d45797be6075effe414b8e0f23
james <jimmindungu3@gmail.com>
2024-09-28 09:35:47 +0300

    configure cors for cross-site requests

--- backend/index.js
+++ backend/index.js
@@ -2,17 +2,33 @@ const express = require("express");
 const mongoose = require("mongoose");
 const User = require("./models/UserSchema.js");
 const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
+
+const maxAge = 3 * 24 * 60 * 60; // maxAge of 3 days in seconds
 
 // environment variables
 const DB_URI = process.env.CONNECTION_STRING;
 const PORT = process.env.PORT || 3000;
+const myJwtSecret = process.env.SECRET_KEY;
 
 const app = express();
 
 // use middlewares
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173", // Replace with your React app's URL
+    credentials: true,
+  })
+);
+app.use(cookieParser());
+
+// Create JWT function
+const createToken = (id) => {
+  return jwt.sign({ id }, myJwtSecret, { expiresIn: maxAge });
+};
 
 // Handle errors that occur during user operations
 const handleErrors = (err) => {
@@ -48,34 +64,43 @@ app.get("/", (req, res) => {
   res.status(200).json({ Message: "Welcome Home" });
 });
 
-// POST route to create a new user
+// Create user route
 app.post("/api/users", async (req, res) => {
   const { fullName, email, password } = req.body;
-
-  // find if email is registered before attempt to save
-  const user = await User.findOne({ email });
-  if (user) {
-    res.status(400).json({ email: "This email is already registered" });
-
-  } else {
-
-    try {
-      // Create a new user instance
-      const newUser = new User({
-        fullName,
-        email,
-        password,
-      });
-
-      // Save the user to the database
-      const savedUser = await newUser.save();
-      res.status(201).json(savedUser);
-      
-    } catch (err) {
-      const errors = handleErrors(err);
-      console.log(errors);
-      res.status(400).json(errors);
+  try {
+    // Check for existing user with the same email
+    const userWithEmail = await User.findOne({ email });
+    if (userWithEmail) {
+      return res.status(400).json({ email: "This email is already in use" });
     }
+    const user = await User.create({ fullName, email, password });
+
+    // Once user is created and saved, create cookie and send it back to client
+    const token = createToken(user._id);
+    res.cookie("jwt", token, {
+      httpOnly: true,
+      maxAge: maxAge * 1000,
+      sameSite: "lax",
+      secure: false, // Note: This requires HTTPS
+    });
+    res.status(201).json({ user: user._id });
+  } catch (err) {
+    const errors = handleErrors(err);
+    res.status(400).json(errors);
+  }
+});
+
+// LOGIN POST request
+app.post("/api/login", async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.login(email, password);
+    const token = createToken(user._id);
+    res.cookie("jwt", token, { maxAge: maxAge * 1000 }); // maxAge in milliseconds
+    res.status(200).json({ user: user._id });
+  } catch (error) {
+    const err = handleErrors(error);
+    res.status(400).json(err);
   }
 });
 
