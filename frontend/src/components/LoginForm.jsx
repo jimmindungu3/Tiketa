56a6af1dfdb4dfc1cb6656aa5436155a53628918
james <jimmindungu3@gmail.com>
2024-09-29 03:31:39 +0300

    handle login

--- frontend/src/components/LoginForm.jsx
+++ frontend/src/components/LoginForm.jsx
@@ -1,34 +1,70 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const LoginForm = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(
+        "http://localhost:3000/api/login",
+        {
+          email,
+          password,
+        },
+        { withCredentials: true }
+      );
+      if (response.status === 200 && response.data.user) {
+        setEmail("");
+        setPassword("");
+        navigate("/");
+      }
+    } catch (error) {
+      console.log(error.response.data); // Log the error to the console
+    }
+  };
+
   return (
     <div className="bg-blue text-white p-6 max-w-lg mx-auto my-16 rounded-lg shadow-lg">
       {/* <h2 className="text-2xl text-white font-semibold mb-6">Login</h2> */}
-      
-      <form>
+
+      <form onSubmit={(e) => handleLogin(e)}>
         {/* Email Field */}
         <div className="mb-4">
-          <label className="block text-bluegray text-sm font-bold mb-2" htmlFor="email">
+          <label
+            className="block text-bluegray text-sm font-bold mb-2"
+            htmlFor="email"
+          >
             Email
           </label>
           <input
             className="text-gray-950 w-full p-2 rounded focus:outline-none focus:ring-2 focus:ring-bluegray"
             type="email"
             id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
             placeholder="example@mail.com"
           />
         </div>
 
         {/* Password Field */}
         <div className="mb-6">
-          <label className="block text-bluegray text-sm font-bold mb-2" htmlFor="password">
+          <label
+            className="block text-bluegray text-sm font-bold mb-2"
+            htmlFor="password"
+          >
             Password
           </label>
           <input
             className="text-gray-950 w-full p-2 rounded focus:outline-none focus:ring-2 focus:ring-bluegray"
             type="password"
             id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
             placeholder="********"
           />
         </div>
@@ -43,7 +79,7 @@ const LoginForm = () => {
           </button>
         </div>
       </form>
-      <hr className='text-white mt-6'/>
+      <hr className="text-white mt-6" />
       <div className="my-4">
         Don't have an account?{" "}
         <span className="font-semibold text-red-100">
@@ -51,7 +87,7 @@ const LoginForm = () => {
         </span>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default LoginForm;
