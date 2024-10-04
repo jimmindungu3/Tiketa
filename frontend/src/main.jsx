a5802d9fc1fe97e97635d5ba593bb4309490a3c7
james <jimmindungu3@gmail.com>
2024-10-04 23:02:05 +0300

    add fetch data, pass events as props

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,30 +1,59 @@
-import { StrictMode } from "react";
+import { StrictMode, useState, useEffect } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import axios from "axios";
 import "./index.css";
 
 // Import pages
 import Home from "./pages/Home.jsx";
 import Sign from "./pages/Sign.jsx";
 import Login from "./pages/Login.jsx";
+import BuyTicketPage from "./pages/BuyTicketPage.jsx";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-  },
-  {
-    path: "/sign-up",
-    element: <Sign />,
-  },
-  {
-    path: "/login",
-    element: <Login />,
-  },
-]);
+const Main = () => {
+  const [events, setEvents] = useState([]);
 
+  // Fetch events when the app starts
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/events")
+      .then((response) => {
+        setEvents(response.data);
+      })
+      .catch((error) => {
+        console.error("There was an error fetching the events!", error);
+      });
+  }, []);
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home events={events} />,
+    },
+    {
+      path: "/sign-up",
+      element: <Sign />,
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+    {
+      path: "/buy-ticket",
+      element: <BuyTicketPage />,
+    },
+    {
+      path: "/book-event/:id", // ID will later be changed to event title
+      element: <BuyTicketPage events={events} />,
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
+};
+
+// Render the app
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <Main />
   </StrictMode>
 );
