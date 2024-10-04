2f4c68e10e8d8926a95d1ae2aa19deb7c03a7354
james <jimmindungu3@gmail.com>
2024-10-04 23:01:21 +0300

    remove fetch events

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,32 +1,16 @@
-import React, { useState, useEffect } from "react";
 import Navbar from "../components/Navbar";
-import EventsSection from "../components/EventsSection";
+import Events from "../components/Events";
 import Footer from "../components/Footer";
 import Slider from "../components/Slider";
-import axios from "axios";
 
-const Home = () => {
-  const [events, setEvents] = useState([]);
-
-  useEffect(() => {
-    // Replace '/path/to/your/events.json' with the actual path to your JSON data
-    axios
-      .get("http://localhost:3001/events")
-      .then((response) => {
-        setEvents(response.data);
-      })
-      .catch((error) => {
-        console.error("There was an error fetching the events!", error);
-      });
-  }, []);
-
+const Home = ({ events }) => {
   return (
-    <div>
+    <>
       <Navbar />
       <Slider />
-      <EventsSection events={events} />
+      <Events events={events} />
       <Footer />
-    </div>
+    </>
   );
 };
 
