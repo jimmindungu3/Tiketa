7fc21b908ecfe0c6ec459c2d0813fce6c6d64720
james <jimmindungu3@gmail.com>
2024-10-04 23:00:08 +0300

    remove fetch events, add Link to buyticket page

--- frontend/src/components/Events.jsx
+++ frontend/src/components/Events.jsx
@@ -1,6 +1,7 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-const Events = ({ events, handleBuyTicket }) => {
+const Events = ({ events }) => {
   return (
     <div className="max-w-6xl mx-auto mt-12 p-4">
       <h1 className="text-3xl font-bold text-red-100 text-center mb-12">
@@ -8,33 +9,32 @@ const Events = ({ events, handleBuyTicket }) => {
       </h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-10">
         {events.map((event) => (
-          <div
-            key={event.id}
-            className="rounded-md overflow-hidden shadow-lg flex flex-col"
-          >
-            <img
-              className="w-full h-64 object-cover rounded-md" /* Adjust height to 64 */
-              src={event.image}
-              alt={event.title}
-            />
-            <div className="p-4 flex flex-col flex-grow justify-between">
-              <div>
-                <h2 className="text-xl font-bold text-gray-900 mb-2">
-                  {event.title}
-                </h2>
-                <p className="text-red-100 text-sm mb-2 font-semibold">
-                  {event.date}
-                </p>
-                <p className="text-bluegray mb-4">{event.venue}</p>
+          <Link key={event.id} to={`/book-event/${event.id}`}>
+            <div
+              key={event.id}
+              className="rounded-md overflow-hidden shadow-lg flex flex-col h-full"
+            >
+              <img
+                className="w-full h-64 object-cover rounded-md"
+                src={event.image}
+                alt={event.title}
+              />
+              <div className="p-4 flex flex-col flex-grow justify-between">
+                <div className="flex-grow">
+                  <h2 className="text-xl font-bold text-gray-900 mb-2">
+                    {event.title}
+                  </h2>
+                  <p className="text-red-100 text-sm mb-2 font-semibold">
+                    {event.date}
+                  </p>
+                  <p className="text-bluegray mb-4">{event.venue}</p>
+                </div>
+                <button className="bg-red-100 py-2 text-center text-white font-semibold rounded-md mt-auto">
+                  BUY TICKET
+                </button>
               </div>
-              <button
-                onClick={() => handleBuyTicket(event)}
-                className="bg-red-100 py-2 text-center text-white font-semibold rounded-md"
-              >
-                BUY TICKET
-              </button>
             </div>
-          </div>
+          </Link>
         ))}
       </div>
     </div>
