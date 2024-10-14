3bd4acc28d3999b260461af4730bbf1e2b526bcf
James Ndungu <jimmindunug3@gmail.com>
2024-10-14 20:53:23 +0300

    delete unusedimports

--- backend/models/EventSchema.js
+++ backend/models/EventSchema.js
@@ -1,5 +1,4 @@
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
 
 // Define EventSchema
 const EventSchema = new mongoose.Schema({
--- frontend/src/pages/BuyTicketPage.jsx
+++ frontend/src/pages/BuyTicketPage.jsx
@@ -1,16 +1,14 @@
 import React from "react";
 import Navbar from "../components/Navbar";
-import Footer from "../components/Footer";
 import BuyTicket from "../components/BuyTicket";
 
-const BuyTicketPage = ({events}) => {
+const BuyTicketPage = ({ events }) => {
   return (
     <div>
       <Navbar />
       <div className="-mt-36 bg-slate-50">
-        <BuyTicket events={events}/>
+        <BuyTicket events={events} />
       </div>
-      
     </div>
   );
 };
