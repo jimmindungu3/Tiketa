a65da44b2ceabfb0a32ca71dd7de428b5a57ad8e
james <jimmindungu3@gmail.com>
2024-10-15 06:36:47 +0300

    permit vercel in cors

--- backend/index.js
+++ backend/index.js
@@ -22,10 +22,11 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://tiketa.vercel.app/"],
     credentials: true,
   })
 );
+
 app.use(cookieParser());
 
 // Create JWT function
