f9819a519dcdde3dde1c9f0924741df2e6344c1d
james <jimmindungu3@gmail.com>
2024-10-15 06:14:11 +0300

    create loader

---
+++ frontend/src/components/Loader.jsx
@@ -0,0 +1,10 @@
+const Loader = () => {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-red-100 border-t-red-200 border-l-transparent border-b-transparent"></div>
+      </div>
+    );
+  };
+  
+  export default Loader;
+  
