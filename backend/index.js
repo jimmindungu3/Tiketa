054d1d17597339af9c974ac6d5c26cb3482316fb
james <jimmindungu3@gmail.com>
2024-10-20 17:04:33 +0300

    allow cors credentials

--- backend/index.js
+++ backend/index.js
@@ -20,22 +20,17 @@ const app = express();
 
 // use middlewares
 app.use(express.json());
-// app.use(
-//   cors({
-//     origin: ["http://localhost:5173", "https://tiketa.vercel.app"],
-//     credentials: true,
-//   })
-// );
+app.use(cookieParser());
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', 'https://tiketa.vercel.app');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
   next();
 });
 
 
-app.use(cookieParser());
 
 // Create JWT function
 const createToken = (id) => {
