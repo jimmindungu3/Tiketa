3ac94aabdbaf4747b116f3b4f792a26902a44962
james <jimmindungu3@gmail.com>
2024-11-16 11:49:29 +0300

    render footer

--- frontend/src/pages/CreateEvent.jsx
+++ frontend/src/pages/CreateEvent.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import EventForm from "../components/EventForm";
+import Footer from "../components/Footer"
 
 const CreateEvent = () => {
   return (
     <div>
       <Navbar />
       <EventForm />
+      <Footer />
     </div>
   );
 };
