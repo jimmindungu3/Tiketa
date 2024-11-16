6c1a10cc9ed338add0426b2bdcbde3bb5fc34ed4
james <jimmindungu3@gmail.com>
2024-11-16 11:38:08 +0300

    protect post event, post event to cloudinary

--- backend/routes/eventRoutes.js
+++ backend/routes/eventRoutes.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const Event = require("../models/EventSchema");
 const cloudinary = require("../utils/cloudinary");
+const jwt = require('jsonwebtoken')
+const myJwtSecret = process.env.SECRET_KEY;
 
 // GET Events route
 router.get("/api/events", async (req, res) => {
@@ -13,42 +15,50 @@ router.get("/api/events", async (req, res) => {
   }
 });
 
-// Create event route
 router.post("/api/events", async (req, res) => {
-  const { title, image, date, description, venue, vip, regular } = req.body;
+  const jwtToken = req.cookies.jwt;
 
-  try {
-    let imageUrl = "";
+  if (jwtToken) {
+    try {
+      const decoded = jwt.verify(jwtToken, myJwtSecret);
 
-    // Upload image to Cloudinary if provided
-    if (image) {
-      const uploadResponse = await cloudinary.uploader.upload(image, {
-        upload_preset: "tiketa",
+      if (!decoded || !decoded.id) {
+        return res.status(401).json({ message: "Unauthorized" });
+      }
+
+      const { title, image, date, description, venue, vip, regular } = req.body;
+
+      let imageUrl = "";
+
+      if (image) {
+        const uploadResponse = await cloudinary.uploader.upload(image, {
+          upload_preset: "tiketa",
+        });
+        imageUrl = uploadResponse.secure_url;
+      }
+
+      const newEvent = new Event({
+        title,
+        image: imageUrl,
+        date,
+        description,
+        venue,
+        vip,
+        regular,
       });
-      imageUrl = uploadResponse.secure_url;
+
+      const savedEvent = await newEvent.save();
+
+      res
+        .status(201)
+        .json({ message: "Event created successfully", savedEvent });
+    } catch (error) {
+      console.error("Error creating event:", error);
+      res.status(500).json({ error: "Internal Server Error" });
     }
-
-    // Create a new event
-    const newEvent = new Event({
-      title,
-      image: imageUrl,
-      date,
-      description,
-      venue,
-      vip,
-      regular,
-    });
-
-    // Save the event to the database
-    const savedEvent = await newEvent.save();
-
-    // Respond with success and the image URL
-    res.status(201).json({ message: "Event created successfully", savedEvent });
-  } catch (error) {
-    console.error("Error creating event:", error);
-    res.status(500).json({ error: "Internal Server Error" });
+  } else {
+    res.status(401).json({ message: "Protected route! Please login" });
   }
 });
-
 
 module.exports = router;
