aeaffe878ced40af0d41fef98271aca02aaf5d47
james <jimmindungu3@gmail.com>
2024-11-16 11:40:46 +0300

    handle event submit

--- frontend/src/components/EventForm.jsx
+++ frontend/src/components/EventForm.jsx
@@ -59,142 +59,116 @@ const EventForm = () => {
   };
 
   return (
-    <div className="bg-white text-blue p-8 max-w-2xl mx-auto my-16 rounded-lg shadow-lg">
+    <div className="bg-blue text-white p-8 max-w-2xl mx-auto my-16 rounded-lg shadow-lg">
       <h2 className="text-2xl font-semibold mb-6">Create New Event</h2>
 
       <form onSubmit={handleSubmit} className="space-y-6">
         {/* Event Banner */}
         <div>
-          <label
-            htmlFor="image"
-            className="block text-bluegray text-sm font-bold mb-2"
-          >
+          <label htmlFor="image" className="block text-sm font-bold mb-2">
             Select Event Banner
           </label>
           <input
+            required
             name="image"
             type="file"
             accept="image/*"
             onChange={handleImageSelection}
-            className="block w-full text-gray-950 file:bg-blue file:text-white file:px-4 file:py-2 file:mr-4 file:rounded-md file:border-none border border-silver rounded-lg focus:outline-none focus:ring-2 focus:ring-safaricomgreen"
+            className="block w-full text-gray-500 bg-white file:bg-gray-600 file:text-white file:px-4 file:py-2 file:mr-4 file:rounded-md file:border-none border border-transparent focus:border-red-200 rounded-lg focus:outline-none"
           />
         </div>
 
         {/* Event Title */}
         <div>
-          <label
-            htmlFor="title"
-            className="block text-bluegray text-sm font-bold mb-2"
-          >
+          <label htmlFor="title" className="block text-sm font-bold mb-2">
             Event Title
           </label>
           <input
+            required
             type="text"
             name="title"
             value={title}
-            onChange={(e) => {
-              setTitle(e.target.value);
-            }}
-            className="w-full p-2 rounded-lg border border-silver focus:outline-none focus:ring-2 focus:ring-safaricomgreen"
+            onChange={(e) => setTitle(e.target.value)}
+            className="w-full p-2 rounded-lg border border-transparent focus:border-red-200  text-gray-950 focus:outline-none"
             placeholder="Enter event title"
           />
         </div>
 
         {/* Event Date */}
         <div>
-          <label
-            htmlFor="date"
-            className="block text-bluegray text-sm font-bold mb-2"
-          >
+          <label htmlFor="date" className="block text-sm font-bold mb-2">
             Select Event Date
           </label>
           <input
+            required
             type="datetime-local"
             name="date"
             value={date}
-            onChange={(e) => {
-              setDate(e.target.value);
-            }}
-            className="w-full p-2 rounded-lg border border-silver focus:outline-none focus:ring-2 focus:ring-safaricomgreen"
+            onChange={(e) => setDate(e.target.value)}
+            className="w-full p-2 rounded-lg border border-transparent focus:border-red-200  text-gray-950 focus:outline-none"
           />
         </div>
 
         {/* Event Description */}
         <div>
-          <label
-            htmlFor="description"
-            className="block text-bluegray text-sm font-bold mb-2"
-          >
+          <label htmlFor="description" className="block text-sm font-bold mb-2">
             Event Description
           </label>
           <textarea
+            required
             name="description"
-            rows="4"
+            rows="5"
             value={description}
-            onChange={(e) => {
-              setDescription(e.target.value);
-            }}
-            className="w-full p-2 rounded-lg border border-silver focus:outline-none focus:ring-2 focus:ring-safaricomgreen"
+            onChange={(e) => setDescription(e.target.value)}
+            className="w-full p-2 rounded-lg border border-transparent focus:border-red-200 text-gray-950 focus:outline-none"
             placeholder="Write a brief description"
           ></textarea>
         </div>
 
         {/* Event Venue */}
         <div>
-          <label
-            htmlFor="venue"
-            className="block text-bluegray text-sm font-bold mb-2"
-          >
+          <label htmlFor="venue" className="block text-sm font-bold mb-2">
             Event Venue
           </label>
           <input
+            required
             type="text"
             name="venue"
             value={venue}
-            onChange={(e) => {
-              setVenue(e.target.value);
-            }}
-            className="w-full p-2 rounded-lg border border-silver focus:outline-none focus:ring-2 focus:ring-safaricomgreen"
+            onChange={(e) => setVenue(e.target.value)}
+            className="w-full p-2 rounded-lg border border-transparent focus:border-red-200 text-gray-950 focus:outline-none"
             placeholder="Enter event venue"
           />
         </div>
 
         {/* Regular Ticket Price */}
         <div>
-          <label
-            htmlFor="regular"
-            className="block text-bluegray text-sm font-bold mb-2"
-          >
+          <label htmlFor="regular" className="block text-sm font-bold mb-2">
             Regular Ticket Price (KES)
           </label>
           <input
+            required
             type="number"
             name="regular"
             value={regular}
-            onChange={(e) => {
-              setRegular(e.target.value);
-            }}
-            className="w-full p-2 rounded-lg border border-silver focus:outline-none focus:ring-2 focus:ring-safaricomgreen"
+            onChange={(e) => setRegular(e.target.value)}
+            className="w-full p-2 rounded-lg border border-transparent focus:border-red-200 text-gray-950 focus:outline-none"
             placeholder="Enter amount"
           />
         </div>
 
         {/* VIP Ticket Price */}
         <div>
-          <label
-            htmlFor="vip"
-            className="block text-bluegray text-sm font-bold mb-2"
-          >
+          <label htmlFor="vip" className="block text-sm font-bold mb-2">
             VIP Ticket Price (KES)
           </label>
           <input
+            required
             type="number"
             name="vip"
             value={vip}
-            onChange={(e) => {
-              setVip(e.target.value);
-            }}
-            className="w-full p-2 rounded-lg border border-silver focus:outline-none focus:ring-2 focus:ring-safaricomgreen"
+            onChange={(e) => setVip(e.target.value)}
+            className="w-full p-2 rounded-lg border border-transparent focus:border-red-200 text-gray-950 focus:outline-none"
             placeholder="Enter amount"
           />
         </div>
@@ -203,7 +177,7 @@ const EventForm = () => {
         <div className="flex justify-between items-center">
           <button
             type="submit"
-            className="bg-safaricomgreen hover:bg-green-700 text-white font-bold py-3 px-6 rounded-lg focus:outline-none focus:ring-2 focus:ring-safaricomgreen transition-all duration-300"
+            className="bg-red-100 hover:bg-red-200 text-white font-bold py-3 px-6 rounded-lg focus:outline-none focus:ring-2 focus:ring-red-100 transition-all duration-300"
           >
             Create Event
           </button>
